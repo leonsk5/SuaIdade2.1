9475fecd91e55caddd31b7f3d10058f0fc54a28f
Leonardo S Melo <leonsk.55@gmail.com>
2020-09-13 18:58:36 -0300

    Update and rename SuaIdade2.1.py to SuaIdade2.2.py
    Mais uma modificação, agora trabalhando com funções.
    Terão muito mais ao longo do tempo.

---
+++ SuaIdade2.2.py
@@ -0,0 +1,65 @@
+"""
+Programa  coleta dados do usuário coletando uma str e add em uma lista, depois
+convertendo para int.
+ABAIXO CRIEI UM PSEUDOCÓDIGO, APESAR DE EXISTER MILHARES. PARA MELHORAR MEU ENTENDIMENTO!
+                           # coletar str
+                           list:
+                           def:input:
+                           if:
+                           elif:
+                           
+                           # coletar int
+                           while:
+                           if:
+                           append:
+                           int:
+                           print:
+                           break:
+                           elif
+                           
+
+"""
+"""
+ATUALIZADO PARA FUNCIONAR COM UMA FUNÇÃO
+LÓGICO TEM BUGS, QUE SERÃO CORRIGIDOS COM O TEMPO!
+
+"""
+
+def suaIdade(nome, ano_nascimento): 
+    lista_nome = [] # lista para adicionar o nomes.
+    lista_ano_nascimento = [] # adicionar anos nascimento.
+    
+    while True: 
+        nome = input("Digite seu nome: ")
+        lista_nome.append(nome) # add input usuário como um string na lista.
+
+        if not nome.isalpha():
+            print("apenas letras")
+        elif len(nome) < 3:
+            print("Nome muito curto.")
+            continue
+        else:
+            break
+    while True:
+        ano_nascimento = input("Digite Ano de Nascimento: ") 
+        lista_ano_nascimento.append(ano_nascimento) # add input usuário com um string na lista
+
+        if len(ano_nascimento) == 4:
+            ano_atual = 2020
+            ano_nascimento = int(ano_nascimento) # conversão de string para int
+            idade = ano_atual - ano_nascimento
+            print(f"{nome} tem {idade} Anos.", lista_nome, lista_ano_nascimento)
+            break
+
+        elif len(ano_nascimento) != 4 or len(ano_nascimento) == "".isnumeric():
+            print("Apenas 4 digitos numericos.")
+            continue
+        return nome, ano_nascimento
+suaIdade(nome=1, ano_nascimento=2)
+
+
+
+
+
+
+
